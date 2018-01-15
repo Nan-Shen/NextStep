86829cc3585918e8fb064ba06274c47c99a9a6e8
Nan-Shen <nanshenbms@gmail.com>
2018-01-14 20:01:58 -0500

    reorganize functions

--- lib/parse.py
+++ lib/parse.py
@@ -10,6 +10,21 @@ import sys
 import pandas as pd
 from datetime import datetime
 
+
+def parse_yahoo_to_nextstep(in_fp, start_date='2013-01-03', end_date='2017-12-29'):
+    """parse yahoo stock price file to nextstep input format
+    in_fp: full file path of yahoo history price of a stock in csv format.
+    start_date: start date of data put into model.
+    end_date: end date of data put into model.
+    You had better check the input file to confirm when to start and when to end.
+    Make sure that stocks compared togethor have similar time range and there are
+    enough training data.
+    """
+    data = parse_input(in_fp, form='yahoo')
+    se_data = start_end(data, start=start_date, end=end_date)
+    ns_data = add_weekday(se_data)
+    return ns_data
+    
 def parse_input(in_fp, form='yahoo'):
     """read input csv file of historical stock price.
     in_fp: full file path of input csv file.
@@ -27,7 +42,7 @@ def parse_input(in_fp, form='yahoo'):
                          'Yahoo format.')
     return data
 
-def start_end(data, start='2013-01-03', end='2017-12-29'):
+def start_end(data, start, end):
     """slice data from start date to end date.
     data: dataframe of stock prices, with 'Date' column in date.time data type
     and all the other column in float.
@@ -52,14 +67,11 @@ def nth_weekday(firstday, day):
                               - days_lastweek) // 7 * 5
     return days_between + days_firstweek + days_lastweek
     
-def add_time_order(data):
-    """add weekday column and nth working day from first day (order of day exclude
-    weekends) column.
+def add_weekday(data):
+    """add weekday column
     data: dataframe of stock prices, with 'Date' column in date.time data type
     and all the other column in float.
     """
     data['Weekday'] = data['Date'].apply(datetime.isoweekday)
-    data = data.sort_values(by=['Date'])
-    data['Order'] = range(data.shape[0])
     return data
     
